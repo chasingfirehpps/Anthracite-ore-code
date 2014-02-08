49954429d1e80f81f5832ff4f11e5319f2fd0199
chasingfirehpps <ijhpps@gmail.com>
2014-02-08 11:18:48 -0500

    Update Anthracite_ore.java

--- Anthracite_ore.java
+++ Anthracite_ore.java
@@ -1,22 +1,33 @@
-package Items;
+package Blocks;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
+import net.minecraft.util.Icon;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class Anthracite extends Item {
+public class Anthracite_ore extends Block{
 	
-	public Anthracite(int id) {
-		super(id);
-		setCreativeTab(CreativeTabs.tabMaterials);
-		setMaxStackSize(64);
-		setUnlocalizedName(Iteminfo.Anthracite_UNLOCALIZED_NAME);
+	public Anthracite_ore(int id) {
+		super(id, Material.rock);
+		setCreativeTab(CreativeTabs.tabBlock);
+		setHardness(4F);
+		setStepSound(Block.soundStoneFootstep);
+		setUnlocalizedName(Blockinfo.Anthracite_ore_UNLOCALIZED_NAME);
 	}
-@Override
-@SideOnly(Side.CLIENT)
-public void registerIcons(IconRegister register){
-	this.itemIcon = register.registerIcon("anthracite"); 
+	@SideOnly(Side.CLIENT)
+	private Icon anthracite_ore;
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister register){
+		this.blockIcon = register.registerIcon("anthracite_ore"); 
+}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public Icon getIcon(int side, int meta) {
+		return anthracite_ore;
 }
 }
