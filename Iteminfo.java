d9f810398dabf2e8f32d22b6cdc91142057591a4
chasingfirehpps <ijhpps@gmail.com>
2014-02-08 16:01:25 -0500

    Create Iteminfo.java
    MinersPicKAxe is commented off

---
+++ Iteminfo.java
@@ -0,0 +1,26 @@
+package Items;
+
+public class Iteminfo {
+//Anthracite
+	
+	public static final String Anthracite_TEXTURE_LOCATION = "items";
+	public static int Anthracite_ID;
+	public static final String Anthracite_KEY = "Anthracite";
+	public static final int Anthracite = 31270;
+	public static final String Anthracite_UNLOCALIZED_NAME = "Anthracite";
+	public static final String Anthracite_NAME = "Anthracite";
+	public static final String Anthracite_Icon = "anthracite";
+	
+//MinersPickAxe
+	//public static final String MinersPickAxe_TEXTURE_LOCATION = "texturefile";
+	
+	//public static int MinersPickAxe_ID;
+	//public static final String MinersPickAxe_KEY = "MinersPickAxe";
+	//public static final int MinersPickAxe = 31271;
+	//public static final String MinersPickAxe_UNLOCALIZED_NAME = "MinersPickAxe";
+	//public static final String MinersPickAxe_NAME = "Miner's Pickaxe";
+	//public static final String MinersPickAxe_Icon = "miner's_pickaxe";
+	
+
+	
+}
