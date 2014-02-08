33183743ede6d213abe595c39feac2641017fec3
chasingfirehpps <ijhpps@gmail.com>
2014-02-08 11:20:09 -0500

    Create Blockinfo.java

---
+++ Blockinfo.java
@@ -0,0 +1,17 @@
+package Blocks;
+
+public class Blockinfo {
+	
+	public static final String TEXTURE_LOCATION = "texturefile";
+	
+	public static int Anthracite_ore_ID = 1620;
+	public static final String Anthracite_ore_KEY = "Anthracite_ore";
+	public static final int Anthracite_ore_DEFAULT = 1360;
+	public static final String Anthracite_ore_UNLOCALIZED_NAME = "Anthracite_ore";
+	public static final String Anthracite_ore_NAME = "Anthracite Ore";
+	
+	public static final String Anthracite_ore = "anthracite_ore";
+
+}
+
+
