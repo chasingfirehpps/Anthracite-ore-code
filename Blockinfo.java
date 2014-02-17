6610081f3752805a3ec468c0517f901629e34259
chasingfirehpps <ijhpps@gmail.com>
2014-02-16 20:40:21 -0500

    Update Blockinfo.java

--- Blockinfo.java
+++ Blockinfo.java
@@ -2,7 +2,7 @@ package Blocks;
 
 public class Blockinfo {
 	
-	public static final String TEXTURE_LOCATION = "texturefile";
+	public static final String TEXTURE_LOCATION = "lotsofcoals";
 	
 	public static int Anthracite_ore_ID = 1620;
 	public static final String Anthracite_ore_KEY = "Anthracite_ore";
@@ -11,7 +11,30 @@ public class Blockinfo {
 	public static final String Anthracite_ore_NAME = "Anthracite Ore";
 	
 	public static final String Anthracite_ore = "anthracite_ore";
+	
+	public static final String Anthracite_ore_TOP="anthracite_ore";
+	public static final String Anthracite_ore_BOT="anthracite_ore";
+	public static final String Anthracite_ore_SIDE="anthracite_ore";
+	
+	//Bituminous
+	
+	public static final String TEXTURE_LOCATION1 = "lotsofcoals";
+	
+	public static int Bituminous_ore_ID = 1621;
+	public static final String Bituminous_ore_KEY = "Bituminous_ore";
+	public static final int Bituminous_ore_DEFAULT = 1361;
+	public static final String Bituminous_ore_UNLOCALIZED_NAME = "Bituminous_ore";
+	public static final String Bituminous_ore_NAME = "Bituminous Ore";
+	
+	public static final String Bituminous_ore = "Bituminous_ore";
+	
+	public static final String Bituminous_ore_TOP="bituminus_ore";
+	public static final String Bituminous_ore_BOT="bituminus_ore";
+	public static final String Bituminous_ore_SIDE="bituminus_ore";
+	
+	
+	
+	
+	
 
 }
-
-
